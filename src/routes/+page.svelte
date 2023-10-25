3360ff2c1c734f38530571fa3fc9cd73fc65186e
a-matyukh <amatyukh@gmail.com>
2023-10-25 16:03:46 +0300

    all info to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,111 @@
-<nav>
+<!-- <nav>
     <a href="/concepts">concepts</a>
     <a href="/experiments">experiments</a>
     <a href="/articles">articles</a>
-</nav>
+</nav> -->
 <div>
-    main page
+    <h3>Concepts</h3>
+    <div>
+        <a href="https://supatupacommander.web.app/" target="_blank">SupatupaCommander</a>
+    </div>
+    <div>
+        <a href="https://www.behance.net/gallery/45198123/Create-your-own-workspace" target="_blank">Create your own workspace</a>
+    </div>
+    <div>
+        <a href="https://medium.com/@amatyukh/reception-of-bruce-almighty-9166d559ed3d" target="_blank">Reception of Bruce</a>
+    </div>
+    <div>
+        <a href="https://www.behance.net/gallery/26521215/What-did-Alexander-Pushkin-14-January-1827" target="_blank">What did Alexander Pushkin 14 January 1827. Raspisnoye IA</a>
+    </div>
+    <div>
+        <a href="https://www.behance.net/gallery/23768213/Ghouse" target="_blank">Ghouse</a>
+    </div>
+    <div>
+        <a href="https://dribbble.com/shots/6753800-I-have-a-JS-power-PinBoard-for-PinWin">PinBoard</a>
+    </div>
+    <div>
+        <a href="#">Muzedi</a>
+    </div>
+</div>
+<h3>Experiments</h3>
+<div>
+    <a href="https://dribbble.com/shots/2324366-Widgets-taskbar" target="_blank">Widgets taskbar</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/3067385-Ghouse" target="_blank">Widget taskbar: Ghouse</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/3063231-Surface-Dial-Music" target="_blank">Surface Dial Music</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/11701964-UpWork-Interface-Refactoring" target="_blank">UpWork Interface Refactoring</a>
+</div>
+<div>
+    <a href="https://www.behance.net/gallery/39786351/Dribbble-Playground" target="_blank">Dribbble Playground</a>
+</div>
+<hr>
+<h4>vocabulAR</h4>
+<div>
+    <a href="https://www.linkedin.com/posts/amatyukh_small-experiment-on-vacation-a-product-idea-activity-7026207879263129600-PF4p?utm_source=share&utm_medium=member_desktop" target="_blank">vocabulAR, part 1</a>
+</div>
+<div>
+    <a href="https://www.linkedin.com/posts/amatyukh_another-vacation-another-experiment-visual-activity-7100132044847898624-yUvA?utm_source=share&utm_medium=member_desktop" target="_blank">vocabulAR, part 2</a>
+</div>
+<hr>
+<h4>opusketch</h4>
+<div>
+    <a href="https://dribbble.com/shots/2529086-Gradual-solo" target="_blank">Gradual solo</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/2529873-Touch-slider" target="_blank">Touch-slider</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/2531791-4-audio-visualisation-mode" target="_blank">4 audio visualisation mode</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/2568733-Main-panel" target="_blank">Main panel</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/2545436-Arrangement-view-features-modes-audio-visualization" target="_blank">Arrangement view features, modes audio visualization</a>
+</div>
+<div>
+    <a href="https://dribbble.com/shots/2543534-Arrangement-view-features-channels-panel" target="_blank">Arrangement view features, channels panel</a>
+</div>
+
+<h3>Articles</h3>
+<div>
+    <a href="https://medium.com/@amatyukh/why-designers-are-already-programmers-69dcd838927e" target="_blank">Why designers are already programmers?</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/development-of-visual-thinking-badb8a8c2d54" target="_blank">Development of visual thinking</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/raker-emulator-animator-64165ece4f" target="_blank">Raker, emulator, animator</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/storydesk-my-non-existent-designing-tool-7dbbb593e927" target="_blank">Storydesk — my non-existent designing-tool</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D0%BA%D0%BE%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D0%BE%D1%80-628b66cf0cac" target="_blank">Конструктор</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D0%B3%D1%80%D0%B0%D0%B1%D0%BB%D0%B8-%D0%B0%D0%BD%D0%B8%D0%BC%D0%B0%D1%82%D0%BE%D1%80-%D1%8D%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80-e4f5f73710e2" target="_blank"># Грабли, аниматор, эмулятор</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D0%BE%D0%BD%D0%B8-%D0%B7%D0%B0%D0%BF%D0%BE%D0%BB%D0%BE%D0%BD%D0%B8%D0%BB%D0%B8-%D0%BC%D0%B5%D0%BD%D1%8F-d138b105c03d" target="_blank">Они заполонили меня!</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D0%B3%D1%80%D0%B0%D0%B1%D0%BB%D0%B8-%D1%8D%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80-%D0%B0%D0%BD%D0%B8%D0%BC%D0%B0%D1%82%D0%BE%D1%80-%D0%B7%D0%B0%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-3e5f01390b04" target="_blank">Грабли, аниматор, эмулятор. Заключение</a>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D1%87%D1%82%D0%BE-%D1%82%D1%8B-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D1%83%D0%B5%D1%88%D1%8C-fc0f3df64b0e" target="_blank">Что ты проектируешь</a>
+</div>
+<hr>
+<div>
+    <a href="https://medium.com/@amatyukh/creative-jam-challenges-42d91dee326b" target="_blank">Creative Jam Challenges</a>
+    <small>игровая площадка, коллективная импровизация</small>
+</div>
+<div>
+    <a href="https://medium.com/@amatyukh/%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5-%D0%B2%D0%B4%D0%BE%D1%85%D0%BD%D0%BE%D0%B2%D0%B5%D0%BD%D0%B8%D0%B5-6e44dcac6722" target="_blank">Активное вдохновение</a>
+    <small>процесс</small>
 </div>
